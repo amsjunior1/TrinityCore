3b43260fc7a2bb113702218c95f7d38c5616de36
Havenard <havenard@hotmail.com>
2015-05-31 16:21:51 -0300

    Fix compile

--- src/server/game/Anticheat/AnticheatData.cpp
+++ src/server/game/Anticheat/AnticheatData.cpp
@@ -36,10 +36,7 @@ void AnticheatData::SetLastOpcode(uint32 opcode)
 
 void AnticheatData::SetPosition(float x, float y, float z, float o)
 {
-    lastMovementInfo.pos.m_positionX = x;
-    lastMovementInfo.pos.m_positionY = y;
-    lastMovementInfo.pos.m_positionZ = z;
-    lastMovementInfo.pos.m_orientation = o;
+    lastMovementInfo.pos.Relocate(x, y, z, o);
 }
 
 uint32 AnticheatData::GetLastOpcode() const
