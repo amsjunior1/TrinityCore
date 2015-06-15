f5521a4be31af11faf55810d5bed5490e3343474
amsjunior1 <amsjunior1@hotmail.com>
2015-06-15 18:37:13 -0300

    arenawatcher fix sql

--- sql/custom/ArenaWatcher/world.sql
+++ sql/custom/ArenaWatcher/world.sql
@@ -1,8 +1,7 @@
 SET @NPC = 50000;
 
 DELETE FROM `creature_template` WHERE `entry` = @NPC;
-INSERT INTO `creature_template` (`entry`, `modelid1`, `name`, `subname`, `faction_A`, `faction_H`, `npcflag`, `unit_class`, `type`, `ScriptName`) VALUES
-(@NPC, '10913', 'Olho espião', 'Arena watcher', '35', '35', '1', '1', '7', 'npc_arena_watcher');
+INSERT INTO `creature_template` (`entry`, `difficulty_entry_1`, `difficulty_entry_2`, `difficulty_entry_3`, `KillCredit1`, `KillCredit2`, `modelid1`, `modelid2`, `modelid3`, `modelid4`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `scale`, `rank`, `dmgschool`, `BaseAttackTime`, `RangeAttackTime`, `BaseVariance`, `RangeVariance`, `unit_class`, `unit_flags`, `unit_flags2`, `dynamicflags`, `family`, `trainer_type`, `trainer_spell`, `trainer_class`, `trainer_race`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `resistance1`, `resistance2`, `resistance3`, `resistance4`, `resistance5`, `resistance6`, `spell1`, `spell2`, `spell3`, `spell4`, `spell5`, `spell6`, `spell7`, `spell8`, `PetSpellDataId`, `VehicleId`, `mingold`, `maxgold`, `AIName`, `MovementType`, `InhabitType`, `HoverHeight`, `HealthModifier`, `ManaModifier`, `ArmorModifier`, `DamageModifier`, `ExperienceModifier`, `RacialLeader`, `questItem1`, `questItem2`, `questItem3`, `questItem4`, `questItem5`, `questItem6`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `flags_extra`, `ScriptName`, `VerifiedBuild`) VALUES ('@NPC', '0', '0', '0', '0', '0', '16112', '0', '0', '0', 'Eye', 'Arena watcher', '', '0', '80', '80', '0', '35', '1', '1', '1.14286', '1', '0', '0', '0', '0', '1', '1', '1', '0', '2048', '0', '0', '0', '0', '0', '0', '7', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '', '0', '3', '1', '1', '1', '1', '1', '1', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', 'npc_arena_watcher', '0')
 
 DELETE FROM `creature_text` WHERE `entry` = @NPC;
 INSERT INTO `creature_text` (`entry`, `groupid`, `text`, `type`, `comment`) VALUES
@@ -13,7 +12,7 @@ INSERT INTO `creature_text` (`entry`, `groupid`, `text`, `type`, `comment`) VALU
 (@NPC, '4', 'O Alvo e GameMaster', '15', 'ArenaWatcher');
 
 DELETE FROM `locales_creature_text` WHERE `entry` = @NPC;
-INSERT INTO `locales_creature_text` (`entry`, `textGroup`, `id`, `text_loc8`) VALUES
+INSERT INTO `locales_creature_text` (`entry`, `Groupid`, `id`, `text_loc8`) VALUES
 (@NPC, '0', '0', '? ?????? ?????? ?????? ???'),
 (@NPC, '1', '0', '???? ??? ????????'),
 (@NPC, '2', '0', '????? ?? ? ????'),
