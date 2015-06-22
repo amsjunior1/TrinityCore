5b59c4aa65a6d3f515bcd2d7ce483ba7cfaf9afc
amsjunior1 <amsjunior1@hotmail.com>
2015-06-22 01:26:19 -0300

    Restore 3rd phase Frost Beacon in Sindragosa encounter

--- src/server/scripts/Northrend/IcecrownCitadel/boss_sindragosa.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/boss_sindragosa.cpp
@@ -496,6 +496,8 @@ class boss_sindragosa : public CreatureScript
                             {
                                 Talk(EMOTE_WARN_FROZEN_ORB, target);
                                 me->CastCustomSpell(SPELL_ICE_TOMB_TARGET, SPELLVALUE_MAX_TARGETS, 1, nullptr, TRIGGERED_FULL_MASK);
+                                me->SetFacingTo(float(2*M_PI));
+                                DoCast(target, SPELL_ICE_TOMB_DUMMY, true);
                             }
                             events.ScheduleEvent(EVENT_ICE_TOMB, urand(16000, 23000));
                             break;
